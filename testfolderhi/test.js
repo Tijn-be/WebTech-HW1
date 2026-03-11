6d7c9bb0df899843cbc4be9a8777cf83cd17420c
Makmatoe <benjamin.tak@outlook.com>
2026-03-11 16:56:48 +0100

    Serve index.html instead of "Hello World" in HTTP server response

--- testfolderhi/test.js
+++ testfolderhi/test.js
@@ -1,8 +1,9 @@
 var http = require("http");
 http
   .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World\n");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    // add index.html file to the same folder and uncomment the line below to serve the file instead of "Hello World"
+    res.write(fs.readFileSync("../index.html"));
   })
   .listen(8004, "localhost");
 console.log("Server running at http://localhost:8004/");
