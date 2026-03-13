c1838127891bc3566461b53612ba00538b83c8d9
Tijn-be <tijn.be@gmail.com>
2026-03-13 14:01:42 +0100

    test switching root

--- testfolderhi/test.js
+++ testfolderhi/test.js
@@ -1,11 +1,43 @@
-var http = require("http");
-var fs = require("fs");
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
-http
-  .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(fs.readFileSync("../index.html"));
-  })
-  .listen(8004, "0.0.0.0");
+const port = 8004;
+const baseDir = path.join(__dirname, "..");
 
-console.log("Server running on port 8004");
+http.createServer((req, res) => {
+
+    let filePath = req.url === "/" ? "/index.html" : req.url;
+    let fullPath = path.join(baseDir, filePath);
+
+    fs.readFile(fullPath, (err, data) => {
+        if (err) {
+            res.writeHead(404);
+            res.end("Bestand niet gevonden: " + filePath);
+            return;
+        }
+
+
+        let ext = path.extname(fullPath);
+        let contentType = "text/html";
+        if (ext === ".css") contentType = "text/css";
+        if (ext === ".js") contentType = "text/javascript";
+
+        res.writeHead(200, { "Content-Type": contentType });
+        res.end(data);
+    });
+}).listen(port, "0.0.0.0");
+
+console.log(`Server draait op poort ${port}`);
+
+// var http = require("http");
+// var fs = require("fs");
+
+// http
+//   .createServer(function (req, res) {
+//     res.writeHead(200, { "Content-Type": "text/html" });
+//     res.end(fs.readFileSync("../index.html"));
+//   })
+//   .listen(8004, "0.0.0.0");
+
+// console.log("Server running on port 8004");
