6de1980b69af3bdc85f11accfd6cec220965a12f
Tijn-be <tijn.be@gmail.com>
2026-03-15 01:56:23 +0100

    Added Team Pages

---
+++ server/Server.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const port = 8004;
+const baseDir = path.join(__dirname, "..");
+
+http.createServer((req, res) => {
+
+    let filePath = req.url === "/" ? "/index.html" : req.url;
+    let fullPath = path.join(baseDir, filePath);
+
+    fs.readFile(fullPath, (err, data) => {
+        if (err) {
+            res.writeHead(404);
+            res.end("Bestand niet gevonden: " + filePath);
+            return;
+        }
+
+        let ext = path.extname(fullPath);
+        let contentType = "text/html";
+        
+        if (ext === ".css") contentType = "text/css";
+        if (ext === ".js") contentType = "text/javascript";
+
+        res.writeHead(200, { "Content-Type": contentType });
+        res.end(data);
+    });
+}).listen(port, "0.0.0.0");
+
+console.log(`Server draait op poort ${port}`);
