168477aa3d8c4997111d617b6f3ac08c4745562f
Tijn-be <tijn.be@gmail.com>
2026-03-26 16:20:35 +0100

    Test password fix6?

--- server/Server.js
+++ server/Server.js
@@ -5,18 +5,49 @@ const sqlite3 = require('sqlite3').verbose();
 
 const port = 8004;
 const baseDir = path.join(__dirname, "..", "F1");
-
 const db = new sqlite3.Database('./users.db');
 
 http.createServer((req, res) => {
 
     //Server
-    let filePath = req.url === "/" ? "/index.html" : req.url;
-    let fullPath = path.join(baseDir, filePath);
-
     res.setHeader("Access-Control-Allow-Origin", "*"); 
     res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    if (req.method === 'OPTIONS') {
+        res.writeHead(204);
+        res.end();
+        return;
+    }
+
+    //Password
+    if (req.method === 'POST' && req.url === '/login') {
+        let body = '';
+        req.on('data', chunk => { body += chunk.toString(); });
+        req.on('end', () => {
+            try {
+                const { username, password } = JSON.parse(body);
+                const query = `SELECT * FROM users WHERE username = ? AND password = ?`;
+                db.get(query, [username, password], (err, row) => {
+                    res.writeHead(200, { "Content-Type": "application/json" });
+                    if (err) {
+                        res.end(JSON.stringify({ success: false, message: "Database error" }));
+                    } else if (row) {
+                        res.end(JSON.stringify({ success: true, user: row.username }));
+                    } else {
+                        res.end(JSON.stringify({ success: false, message: "Wrong username or password" }));
+                    }
+                });
+            } catch (e) {
+                res.writeHead(400);
+                res.end("Invalid JSON");
+            }
+        });
+        return;
+    }
+    
+    let filePath = req.url === "/" ? "/index.html" : req.url;
+    let fullPath = path.join(baseDir, filePath);
 
     fs.readFile(fullPath, (err, data) => {
         if (err) {
@@ -27,36 +58,15 @@ http.createServer((req, res) => {
 
         let ext = path.extname(fullPath);
         let contentType = "text/html";
-        
         if (ext === ".css") contentType = "text/css";
         if (ext === ".js") contentType = "text/javascript";
 
         res.writeHead(200, { "Content-Type": contentType });
         res.end(data);
     });
-}).listen(port, "0.0.0.0", () => { console.log(`Server draait op poort ${port}`);
 
-    //Password
-    if (req.method === 'POST' && req.url === '/login') {
-        let body = '';
-        req.on('data', chunk => { body += chunk.toString(); });
-        req.on('end', () => {
-            const { username, password } = JSON.parse(body);
-
-            const query = `SELECT * FROM users WHERE username = ? AND password = ?`;
-            db.get(query, [username, password], (err, row) => {
-              res.writeHead(200, { "Content-Type": "application/json" });
-                if (err) {
-                    res.end(JSON.stringify({ success: false, message: "Database error" }));
-                } else if (row) {
-                    res.end(JSON.stringify({ success: true, user: row.username }));
-                } else {
-                    res.end(JSON.stringify({ success: false, message: "Wrong username or password" }));
-                }
-            });
-        });
-        return;
-    }
+}).listen(port, "0.0.0.0", () => 
+    { console.log(`Server draait op poort ${port}`);
 });
 
 //Makes db
