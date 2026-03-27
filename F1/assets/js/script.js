5ab0bbeeea1014a828da1a296b61f2d9597c1c3a
Tijn-be <tijn.be@gmail.com>
2026-03-27 11:09:22 +0100

    Test password fix11?

--- F1/assets/js/script.js
+++ F1/assets/js/script.js
@@ -13,7 +13,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     return;
   }
 
-  fetch('/api/login', {
+  fetch('login', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ username: username, password: password })
